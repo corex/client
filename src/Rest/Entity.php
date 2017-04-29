4e691da3e7dd37f07cddecb006ad2fbe6420b20a
CoRex <dev@corex.dk>
2017-04-29 16:06:50 +0200

    Changed Entity to use Properties from support package.

--- src/Rest/Entity.php
+++ src/Rest/Entity.php
@@ -2,55 +2,8 @@
 
 namespace CoRex\Client\Rest;
 
-abstract class Entity
+use CoRex\Support\Properties;
+
+abstract class Entity extends Properties
 {
-    /**
-     * Data constructor.
-     *
-     * @param mixed $data
-     */
-    public function __construct($data)
-    {
-        if ($data === null) {
-            $data = [];
-        }
-        $properties = $this->getProperties();
-        if (count($properties) > 0) {
-            foreach ($properties as $property) {
-                $value = null;
-                if (isset($data[$property])) {
-                    $value = $data[$property];
-                }
-                $this->{$property} = $value;
-            }
-        }
-    }
-
-    /**
-     * To array.
-     *
-     * @return array
-     */
-    public function toArray()
-    {
-        return get_object_vars($this);
-    }
-
-    /**
-     * Get properties.
-     *
-     * @return array
-     */
-    private function getProperties()
-    {
-        $reflectionClass = new \ReflectionClass(get_class($this));
-        $properties = $reflectionClass->getProperties();
-        $result = [];
-        if (count($properties) > 0) {
-            foreach ($properties as $property) {
-                $result[] = $property->name;
-            }
-        }
-        return $result;
-    }
 }
